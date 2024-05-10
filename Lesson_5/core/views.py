c4b63ca9baf69877c9b096c932e511af449d8b30
ildar <ahmetgalin989@gmail.com>
2024-05-11 02:20:04 +0500

    template_ch1

--- Lesson_5/core/views.py
+++ Lesson_5/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return HttpResponse('Hello!')
+    posts = Post.objects.all()
+    return render(request, template_name='core/index.html', context={'posts' : posts, 'title':'Список статей'})
--- Lesson_5/project/urls.py
+++ Lesson_5/project/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from project.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
