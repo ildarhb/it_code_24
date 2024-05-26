e4066ba36d84a9d84b64919c5d3a9f120458ac09
ildar <ahmetgalin989@gmail.com>
2024-05-26 14:00:50 +0500

    фильтры

--- Lesson_5/core/filters.py
+++ Lesson_5/core/filters.py
@@ -1,8 +1,17 @@
 import django_filters
 
-from core.models import Post
+from core.models import Post, Category
 
 class PostFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter("Заголовок", lookup_expr='icontains')
+    date = django_filters.DateFilter(field_name='date', label="Дата", lookup_expr='lt')
+    like_from = django_filters.NumberFilter(field_name='like', label="Лайки", lookup_expr='gt')
+    class Meta:
+        model = Post
+        exclude = ('photo',)
+
+class CategoryFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter("Заголовок", lookup_expr='icontains')
     class Meta:
         model = Post
         exclude = ('photo',)
